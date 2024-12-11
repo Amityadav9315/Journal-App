96039003a83b6c17d0725f5b66d41057ac274e7e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-11 11:40:38 +0530

    Journal app

--- src/main/java/net/engineeringdigest/journalApp/repository/JournalEntryRepository.java
+++ src/main/java/net/engineeringdigest/journalApp/repository/JournalEntryRepository.java
@@ -4,4 +4,7 @@ import net.engineeringdigest.journalApp.entity.JournalEntry;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface JournalEntryRepository extends MongoRepository<JournalEntry,String> {
+
+
+
 }
