6c307649c5d1734c16c3d69f266c7241c1675b74
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-16 00:43:26 +0530

    journal entry

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -2,6 +2,10 @@ package net.engineeringdigest.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -11,6 +15,11 @@ public class JournalApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(JournalApplication.class, args);
+	}
+	@Bean
+	public PlatformTransactionManager falana(MongoDatabaseFactory dbFactory){
+		return  new MongoTransactionManager(dbFactory);
+
 	}
 
 }
