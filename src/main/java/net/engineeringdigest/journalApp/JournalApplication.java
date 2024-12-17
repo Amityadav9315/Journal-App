80a1b671b8bc7ef929fd380c0b129cc7d01add7b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-17 13:17:45 +0530

    fblfs

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 public class JournalApplication {
+	sout
 
 	public static void main(String[] args) {
 
